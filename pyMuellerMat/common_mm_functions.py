03c7c12afd135cdbaf9b9a58ef95ce759c0ce054
Emily Park <eppark@caltech.edu>
2019-09-16 09:10:50 -0700

    Added second diattenuating retarder (Witzel). Fixed minor errors.

--- pyMuellerMat/common_mm_functions.py
+++ pyMuellerMat/common_mm_functions.py
@@ -7,82 +7,87 @@ All sign conventions and coordinate definitions follow Golstein's book on Polari
 
 import numpy as np
 
+
 #######################################
 ############# Polarizers ##############
 #######################################
 
 #### The functions that define the mueller matrices ####
-def general_polarizer_function(px=1.,py=1.):
-	'''
-	The mueller matrix for a general polarizer. 
-	The default kwargs px=py=1 returns a identity matrix.
+def general_polarizer_function(px=1., py=1.):
+    '''
+    The mueller matrix for a general polarizer.
+    The default kwargs px=py=1 returns a identity matrix.
 
-	Goldstein Eq'm 5-9
-	Kwargs:
-	px	- The transmission of polarization along the x-axis
-	py  - The transmission of polarization along the y-axis
-	'''
-	mm = 0.5*np.array([[px**2+py**2, px**2-py**2, 0,      0],
-					  [px**2-py**2, px**2+py**2, 0,      0],
-					  [0,           0,           2*px*py,0],
-					  [0,           0,           0,      2*px*py]])
-	return mm
+    Goldstein Eq'm 5-9
+    Kwargs:
+    px	- The transmission of polarization along the x-axis
+    py  - The transmission of polarization along the y-axis
+    '''
+    mm = 0.5 * np.array([[px ** 2 + py ** 2, px ** 2 - py ** 2, 0, 0],
+                         [px ** 2 - py ** 2, px ** 2 + py ** 2, 0, 0],
+                         [0, 0, 2 * px * py, 0],
+                         [0, 0, 0, 2 * px * py]])
+    return mm
+
 
 def horizontal_polarizer_function():
-	'''
-	The mueller matrix for a horizontal polarizer
-	The default kwarg px=1 returns an ideal horizontal polarizer. 
+    '''
+    The mueller matrix for a horizontal polarizer
+    The default kwarg px=1 returns an ideal horizontal polarizer.
 
-	Goldstein Eq'n 5-12
+    Goldstein Eq'n 5-12
 
-	Kwargs:
-	px	- The transmission of polarization along the x-axis
-	'''
-	mm = general_polarizer_function(px=1.,py=0.)
+    Kwargs:
+    px	- The transmission of polarization along the x-axis
+    '''
+    mm = general_polarizer_function(px=1., py=0.)
 
-	return mm
+    return mm
+
 
 def vertical_polarizer_function():
-	'''
-	The mueller matrix for a vertical polarizer
-	The default kwarg py=1 returns an ideal vertical polarizer. 
+    '''
+    The mueller matrix for a vertical polarizer
+    The default kwarg py=1 returns an ideal vertical polarizer.
 
-	Goldstein Eq'n 5-14 (well.. close to it)
+    Goldstein Eq'n 5-14 (well.. close to it)
 
-	Kwargs:
-	py	- The transmission of polarization along the x-axis
-	'''
-	mm = general_polarizer_function(px=0.,py=1.)
+    Kwargs:
+    py	- The transmission of polarization along the x-axis
+    '''
+    mm = general_polarizer_function(px=0., py=1.)
 
-	return mm
+    return mm
 
-def wollaston_prism_function(beam='o',eta=1.):
-	'''
-	A function that returns mueller matrix for an ideal wollaston prism. 
 
-	Goldstein Eq'n 5-18
+def wollaston_prism_function(beam='o', eta=1.):
+    '''
+    A function that returns mueller matrix for an ideal wollaston prism.
 
-	kwargs:
-	beam	- Equal to 'o' or 'e', corresponding to ordinary and extraordinary beams
-	eta 	- A modulation efficiency term. 
-	'''
-	if (beam != 'o') & (beam !='e'):
-		print("For a wollaston prism you must specify a beam of either 'o' or 'e'.")
-		print("Assumin you want 'o' for now.")
-		beam = 'o'
+    Goldstein Eq'n 5-18
 
-	#If the ordinary beam then sign = 1
-	if beam == 'o':
-		sign = eta
-	#If the extraordinary beam then sign = -1
-	else: 
-		sign = -eta
+    kwargs:
+    beam	- Equal to 'o' or 'e', corresponding to ordinary and extraordinary beams
+    eta 	- A modulation efficiency term.
+    '''
+    if (beam != 'o') & (beam != 'e'):
+        print("For a wollaston prism you must specify a beam of either 'o' or 'e'.")
+        print("Assuming you want 'o' for now.")
+        beam = 'o'
 
-	mm = 0.5*np.array([[1,sign,0,0],
-					  [sign,1,0,0],
-					  [0,0,0,0],
-					  [0,0,0,0]])
-	return mm
+    # If the ordinary beam then sign = 1
+    if beam == 'o':
+        sign = eta
+    # If the extraordinary beam then sign = -1
+    else:
+        sign = -eta
+
+    mm = 0.5 * np.array([[1, sign, 0, 0],
+                         [sign, 1, 0, 0],
+                         [0, 0, 0, 0],
+                         [0, 0, 0, 0]])
+    return mm
+
 
 ######################################
 ############# Retarders ##############
@@ -90,35 +95,38 @@ def wollaston_prism_function(beam='o',eta=1.):
 
 #### The functions that define the mueller matrices ####
 def general_retarder_function(phi=0.):
-	'''
-	The mueller matrix for a general retarder. 
-	The default kwargs phi=0 corresponds to the identity matrix
+    '''
+    The mueller matrix for a general retarder.
+    The default kwargs phi=0 corresponds to the identity matrix
 
-	Golstein Eq'n 5-27
-	Kwargs:
-	phi	-	The phase retardance between the x and y components in radians
-	'''
-	mm = np.array([[1,0, 0,           0],
-				  [0,1, 0,           0],
-				  [0,0, np.cos(phi), np.sin(phi)],
-				  [0,0,-np.sin(phi), np.cos(phi)]])
+    Golstein Eq'n 5-27
+    Kwargs:
+    phi	-	The phase retardance between the x and y components in radians
+    '''
+    mm = np.array([[1, 0, 0, 0],
+                   [0, 1, 0, 0],
+                   [0, 0, np.cos(phi), np.sin(phi)],
+                   [0, 0, -np.sin(phi), np.cos(phi)]])
 
-	return mm
+    return mm
+
 
 def halfwave_retarder_function():
-	'''
-	The mueller matrix for a half-wave retarder
+    '''
+    The mueller matrix for a half-wave retarder
 
-	Goldstein Eq'n 5-32
-	'''
-	return general_retarder_function(phi=np.pi)
+    Goldstein Eq'n 5-32
+    '''
+    return general_retarder_function(phi=np.pi)
+
 
 def quarterwave_retarder_function():
-	'''
-	The mueller matrix for a quarter-wave retarder. 
-	Goldstein Eq'n 5-28
-	'''
-	return general_retarder_function(phi=np.pi/2.)
+    '''
+    The mueller matrix for a quarter-wave retarder.
+    Goldstein Eq'n 5-28
+    '''
+    return general_retarder_function(phi=np.pi / 2.)
+
 
 #################################
 ############# MISC ##############
@@ -126,38 +134,53 @@ def quarterwave_retarder_function():
 
 #### The functions that define the mueller matrices ####
 def rotator_function(pa=0.):
-	'''
-	The mueller matrix for a rotator. 
-	The rotation theta defined from the positive x-axis in the direction of the y-axis. 
+    '''
+    The mueller matrix for a rotator.
+    The rotation theta defined from the positive x-axis in the direction of the y-axis.
 
-	Goldstein Eq'n 5-41
+    Goldstein Eq'n 5-41
 
-	Kwargs:
-	pa	-	The physical position angle of rotation in degrees. Note, the function will rotate the polarization by 2*pa
-	'''
+    Kwargs:
+    pa	-	The physical position angle of rotation in degrees. Note, the function will rotate the polarization by 2*pa
+    '''
 
-	pa_rad = np.radians(pa)
+    pa_rad = np.radians(pa)
 
-	mm = np.array([[1, 0,           0,           0],
-				  [0, np.cos(2*pa_rad),np.sin(2*pa_rad),0],
-				  [0,-np.sin(2*pa_rad),np.cos(2*pa_rad),0],
-				  [0, 0,           0,           1]])
-	return mm
+    mm = np.array([[1, 0, 0, 0],
+                   [0, np.cos(2 * pa_rad), np.sin(2 * pa_rad), 0],
+                   [0, -np.sin(2 * pa_rad), np.cos(2 * pa_rad), 0],
+                   [0, 0, 0, 1]])
+    return mm
+
 
 def diattenuator_retarder_function(epsilon=1, phi=0.):
-	'''
-	The function that describes a linear diattenuator that has a retardance
+    '''
+    The function that describes a linear diattenuator that has a retardance (Goldstein)
 
-	Inputs: 
-	epsilon -	The diattenuation
-	phi		-	The phase retardance between the x and y components in radians
+    Inputs:
+    epsilon -	The diattenuation
+    phi		-	The phase retardance between the x and y components in radians
 
-	'''
-	mm = np.array([[1, epsilon, 0, 0],
-					   [epsilon, 1, 0, 0],
-					   [0,		   0,		  np.sqrt(1-epsilon**2)*np.cos(phi), np.sqrt(1-epsilon**2)*np.sin(phi)],
-					   [0,		   0,		  -np.sqrt(1-epsilon**2)*np.sin(phi), np.sqrt(1-epsilon**2)*np.cos(phi)]])
-	return mm
+    '''
+    mm = np.array([[1, epsilon, 0, 0],
+                   [epsilon, 1, 0, 0],
+                   [0, 0, np.sqrt(1 - epsilon ** 2) * np.cos(phi), np.sqrt(1 - epsilon ** 2) * np.sin(phi)],
+                   [0, 0, -np.sqrt(1 - epsilon ** 2) * np.sin(phi), np.sqrt(1 - epsilon ** 2) * np.cos(phi)]])
+    return mm
 
 
+def diattenuator_retarder_function2(r1=1., r2=0., delta=0.):
+    '''
+    The function that describes a linear diattenuator that has a retardance (Witzel)
 
+    Inputs:
+    r1      -  	The first reflection coefficient (perpendicular)
+    r2      -	The second reflection coefficient (parallel)
+    delta   -   The relative retardance between the components
+
+    '''
+    mm = np.array([[0.5 * (r1 + r2), 0.5 * (r1 - r2), 0, 0],
+                   [0.5 * (r1 - r2), 0.5 * (r1 + r2), 0, 0],
+                   [0, 0, np.sqrt(r1 * r2) * np.cos(delta), -np.sqrt(r1 * r2) * np.sin(delta)],
+                   [0, 0, np.sqrt(r1 * r2) * np.sin(delta), np.sqrt(r1 * r2) * np.cos(delta)]])
+    return mm
--- pyMuellerMat/common_mms.py
+++ pyMuellerMat/common_mms.py
@@ -7,65 +7,80 @@ All sign conventions and coordinate definitions follow Golstein's book on Polari
 '''
 
 import numpy as np
-from pyMuellerMat.common_mm_functions import * 
+from pyMuellerMat.common_mm_functions import *
 from pyMuellerMat import MuellerMat
+
 
 #######################################
 ############# Polarizers ##############
 #######################################
 
 class Polarizer(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a general polarizer
-	'''
-	def __init__(self,name='Polarizer'):
-		super(Polarizer, self).__init__(general_polarizer_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a general polarizer
+    '''
+
+    def __init__(self, name='Polarizer'):
+        super(Polarizer, self).__init__(general_polarizer_function, name=name)
+
 
 class HorizontalPolarizer(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a horizontal polarizer
-	'''
-	def __init__(self,name='HorizontalPolarizer'):
-		super(HorizontalPolarizer, self).__init__(horizontal_polarizer_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a horizontal polarizer
+    '''
+
+    def __init__(self, name='HorizontalPolarizer'):
+        super(HorizontalPolarizer, self).__init__(horizontal_polarizer_function, name=name)
+
 
 class VerticalPolarizer(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a vertical polarizer
-	'''
-	def __init__(self,name='VerticalPolarizer'):
-		super(VerticalPolarizer, self).__init__(vertical_polarizer_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a vertical polarizer
+    '''
+
+    def __init__(self, name='VerticalPolarizer'):
+        super(VerticalPolarizer, self).__init__(vertical_polarizer_function, name=name)
+
 
 class WollastonPrism(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a wollaston prism
-	'''
-	def __init__(self,name='WollastonPrism'):
-		super(WollastonPrism, self).__init__(wollaston_prism_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a wollaston prism
+    '''
+
+    def __init__(self, name='WollastonPrism'):
+        super(WollastonPrism, self).__init__(wollaston_prism_function, name=name)
+
 
 ######################################
 ############# Retarders ##############
 ######################################
 
 class Retarder(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a general polarizer
-	'''
-	def __init__(self,name='Retarder',phi=0):
-		super(Retarder, self).__init__(general_retarder_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a general polarizer
+    '''
+
+    def __init__(self, name='Retarder', phi=0):
+        super(Retarder, self).__init__(general_retarder_function, name=name)
+
 
 class HWP(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a half-wave polarizer
-	'''
-	def __init__(self,name='HalfwaveRetarder'):
-		super(HWP, self).__init__(halfwave_retarder_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a half-wave polarizer
+    '''
+
+    def __init__(self, name='HalfwaveRetarder'):
+        super(HWP, self).__init__(halfwave_retarder_function, name=name)
+
 
 class QWP(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a quarter-wave polarizer
-	'''
-	def __init__(self,name='QuarterwaveRetarder'):
-		super(QWP, self).__init__(quarterwave_retarder_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a quarter-wave polarizer
+    '''
+
+    def __init__(self, name='QuarterwaveRetarder'):
+        super(QWP, self).__init__(quarterwave_retarder_function, name=name)
+
 
 #################################
 ############# MISC ##############
@@ -73,17 +88,27 @@ class QWP(MuellerMat.MuellerMatrix):
 
 #### The MuellerMatrix child classes ####
 class Rotator(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a general rotator
-	'''
-	def __init__(self,name='Rotator'):
-		super(Rotator, self).__init__(rotator_function,name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a general rotator
+    '''
+
+    def __init__(self, name='Rotator'):
+        super(Rotator, self).__init__(rotator_function, name=name)
+
 
 class DiattenuatorRetarder(MuellerMat.MuellerMatrix):
-	'''
-	A MuellerMat.MuellerMatrix child class for a diattenuator retarder
-	'''
-	def __init__(self, name='DiattenuatorRetarder'):
-		super(DiattenuatorRetarder,self).__init__(diattenuator_retarder_function, name=name)
+    '''
+    A MuellerMat.MuellerMatrix child class for a diattenuator retarder (Goldstein)
+    '''
+
+    def __init__(self, name='DiattenuatorRetarder'):
+        super(DiattenuatorRetarder, self).__init__(diattenuator_retarder_function, name=name)
 
 
+class DiattenuatorRetarder2(MuellerMat.MuellerMatrix):
+    '''
+    A MuellerMat.MuellerMatrix child class for a diattenuator retarder (Witzel)
+    '''
+
+    def __init__(self, name='DiattenuatorRetarder2'):
+        super(DiattenuatorRetarder2, self).__init__(diattenuator_retarder_function2, name=name)
