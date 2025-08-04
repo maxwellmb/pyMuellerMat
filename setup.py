7eb043611a86690d97b11f418cae8aa211d64611
thomasmcintoshh <thomasmc@Mueller.physics.ucsb.edu>
2025-08-04 13:50:07 -0700

    fixed imports

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from setuptools import setup,find_packages
+from setuptools import setup, find_packages
 
 setup(
     name="pyMuellerMat",
     version="0.1",
-    packages=find_packages(include=["pyMuellerMat*", "physical_models*"]),
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={
+        "pyMuellerMat.physical_models": ["physical_model_csvs/*.csv"]
+    },
 )
